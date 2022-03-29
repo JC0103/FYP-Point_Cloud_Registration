e0dea2bffa1d19dd3fc347dbb5de7fe1639ee70f
JC Chia <jingcheng19986630@outlook.com>
2022-03-29 18:58:09 +0800

    Added second servo

--- tilt_module_controller/servo_ros/servo_ros.ino
+++ tilt_module_controller/servo_ros/servo_ros.ino
@@ -6,13 +6,17 @@
 #include <std_msgs/Bool.h>
 
 
-Servo futaba_servo;  // create servo object to control a servo
+Servo L_servo, R_servo;  // create servo object to control a servo
 
 
-int angle;    // servo motor angle
+int angle; // servo motor angle
 const int min_angle = 0;
 const int max_angle = 180;
 unsigned long previous_write_time = -999;
+
+// For callibration
+const int L_zeros_angle = 0;
+const int R_zeros_angle = 0;
 
 //Function declaration
 void servoAngleCB(const std_msgs::Int16& msg);
@@ -27,7 +31,8 @@ ros::Publisher arduino_hb_pub("servo/hb", &hb_msg);
 void setup() {
   Serial.begin(115200);
   nh.initNode();  //initialize ros node handler
-  futaba_servo.attach(9);  // attaches the servo on pin 9 to the servo object
+  L_servo.attach(9);  // attaches the servos on pin 9, 10 to the servo object
+  R_servo.attach(10);
   nh.subscribe(servo_angle_sub);
   nh.advertise(arduino_hb_pub);
 }
@@ -36,14 +41,15 @@ void loop() {
   // waits for the servo to get there (0.22 sec/60°at 4.8V)
   if (abs(millis() - previous_write_time) > 600){
     arduino_hb_pub.publish(&hb_msg);
-    futaba_servo.write(angle);                  // sets the servo position according to the scaled value
+    L_servo.write(angle);                  // sets the servo position according to the scaled value
   }                      
   nh.spinOnce();
   delay(3);
 }
 
 void servoAngleCB(const std_msgs::Int16& msg){
-    if (msg.data < min_angle && msg.data > max_angle){
+    if ((msg.data + L_zeros_angle < min_angle && msg.data + L_zeros_angle > max_angle) ||
+    (msg.data + R_zeros_angle < min_angle && msg.data + R_zeros_angle > max_angle)){
         nh.logwarn("The input angle is out of range");
     }
     else angle = msg.data;
