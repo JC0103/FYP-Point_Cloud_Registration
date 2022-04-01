2c8913bdda79ffa37b286acfe5c04c12f4769436
JC Chia <jingcheng19986630@outlook.com>
2022-04-01 10:32:47 +0800

    updated topic name

--- tilt_module_controller/servo_ros/servo_ros.ino
+++ tilt_module_controller/servo_ros/servo_ros.ino
@@ -15,8 +15,8 @@ const int max_angle = 180;
 unsigned long previous_write_time = -999;
 
 // For callibration
-const int L_zeros_angle = 0;
-const int R_zeros_angle = 0;
+const int L_zeros_angle = 37;
+const int R_zeros_angle = 140;
 
 //Function declaration
 void servoAngleCB(const std_msgs::Int16& msg);
@@ -24,7 +24,7 @@ void servoAngleCB(const std_msgs::Int16& msg);
 
 //ROS communications
 ros::NodeHandle nh;
-ros::Subscriber <std_msgs::Int16> servo_angle_sub("servo/angle", &servoAngleCB);
+ros::Subscriber <std_msgs::Int16> servo_angle_sub("multiastra_subscriber1/servo/angle", &servoAngleCB);
 std_msgs::Empty hb_msg;
 ros::Publisher arduino_hb_pub("servo/hb", &hb_msg);
 
@@ -41,7 +41,8 @@ void loop() {
   // waits for the servo to get there (0.22 sec/60°at 4.8V)
   if (abs(millis() - previous_write_time) > 600){
     arduino_hb_pub.publish(&hb_msg);
-    L_servo.write(angle);                  // sets the servo position according to the scaled value
+    L_servo.write(angle + L_zeros_angle);                  // sets the servo position according to the scaled value
+    R_servo.write(R_zeros_angle - angle);
   }                      
   nh.spinOnce();
   delay(3);
@@ -49,7 +50,7 @@ void loop() {
 
 void servoAngleCB(const std_msgs::Int16& msg){
     if ((msg.data + L_zeros_angle < min_angle && msg.data + L_zeros_angle > max_angle) ||
-    (msg.data + R_zeros_angle < min_angle && msg.data + R_zeros_angle > max_angle)){
+    (R_zeros_angle - msg.data < min_angle &&  R_zeros_angle - msg.data > max_angle)){
         nh.logwarn("The input angle is out of range");
     }
     else angle = msg.data;
